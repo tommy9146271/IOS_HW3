484137b0350ec14ced4342d7b645ab315008fdd4
tommy9146271 <tommy9146271@icloud.com>
2022-11-10 23:34:05 +0800

    Initial Commit

---
+++ HW3/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HW3
+//
+//  Created by 李子暘 on 2022/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HW3/HW3App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3App.swift
+//  HW3
+//
+//  Created by 李子暘 on 2022/11/10.
+//
+
+import SwiftUI
+
+@main
+struct HW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
